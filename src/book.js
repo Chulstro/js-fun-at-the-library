e0c65ac9155d88f338a09a5b304e4a1569243834
Chandler Hulstrom <chulstro11@gmail.com>
2021-06-04 12:49:05 -0600

    write book tests pass in book functionality

--- src/book.js
+++ src/book.js
@@ -22,11 +22,20 @@ function calculatePageCount(title) {
   return title.length * 20
 }
 
+function writeBook(title, character, genre) {
+  return {
+    title: title,
+    mainCharacter: character,
+    pageCount: calculatePageCount(title),
+    genre: genre
+  }
+}
+
 module.exports = {
   createTitle,
   buildMainCharacter,
   saveReview,
   calculatePageCount,
-  // writeBook,
+  writeBook,
   // editBook
 }
