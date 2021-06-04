13fa5bd7d049a98dd28fb724df83d4bfb0adfeea
Chandler Hulstrom <chulstro11@gmail.com>
2021-06-04 14:06:55 -0600

    Unshelf functionality for shelf tests complete

--- src/shelf.js
+++ src/shelf.js
@@ -1,12 +1,19 @@
 function shelfBook(book, shelf) {
   if (shelf.length < 3)
-    shelf.unshift(book)
-  return shelf
+    shelf.unshift(book);
+}
+
+function unshelfBook(title, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title.includes(title)) {
+      shelf.splice(i, 1);
+    }
+  }
 }
 
 module.exports = {
   shelfBook,
-  // unshelfBook,
+  unshelfBook,
   // listTitles,
   // searchShelf
 };
