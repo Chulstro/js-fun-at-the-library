14b5b9bd0ad234613e09922425a165b92011a419
Chandler Hulstrom <chulstro11@gmail.com>
2021-06-06 07:20:56 -0600

    Book checkout functionality complete in library tests

--- src/Library.js
+++ src/Library.js
@@ -1,4 +1,7 @@
-function createLibrary (name) {
+const searchShelf = require('./shelf').searchShelf;
+const unshelfBook = require('./shelf').unshelfBook;
+
+function createLibrary(name) {
   return {
     name: name,
     shelves: {
@@ -13,8 +16,19 @@ function addBook(library, book) {
   library.shelves[book.genre].unshift(book)
 }
 
+function checkoutBook(library, title, shelfName) {
+  shelf = library.shelves[shelfName]
+  if (searchShelf(shelf, title)) {
+    unshelfBook(title, shelf)
+    return `You have now checked out ${title} from the ${library.name}`
+  }
+  else {
+    return `Sorry, there are currently no copies of ${title} available at the ${library.name}`
+  }
+}
+
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook
+  checkoutBook
 };
